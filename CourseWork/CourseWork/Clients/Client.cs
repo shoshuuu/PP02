263aa408f82aa5b6768e3a34a3eb154ae25bc9a4
shoshuu <58806867+shoshuuu@users.noreply.github.com>
2022-03-21 11:49:48 +0300

    Add files via upload

---
+++ CourseWork/CourseWork/Clients/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseWork.Clients
+{
+    class Client
+    {
+        public string FirstName { get; set; }
+        public string LasttName { get; set; }
+        public string Patronymic { get; set; }
+        public string Adress { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Regular { get; set; }
+        public string Discount { get; set; }
+    }
+}
---
+++ CourseWork/CourseWork/Clients/ClientsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseWork.Clients
+{
+    class ClientsContext : DbContext
+    {
+        public ClientsContext() : base("")
+        {
+
+        }
+
+        public DbSet<Client> Clients { get; set; }
+    }
+}
